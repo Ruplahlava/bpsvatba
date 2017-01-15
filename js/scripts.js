8bcac2c92e872dbf7b321888cff07aff7db9468b
Stepan Postranecky <stepan.postranecky@gmail.com>
2017-01-15 17:10:34 +0100

    Added most content Lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         verticalCentered: true,
         menu: '#menu',
-        anchors: ['uvod', 'info', 'doprava', 'pozvanka', 'radost']
+        anchors: ['uvod', 'info', 'doprava', 'pozvanka', 'radost'],
+        navigation: true,
+        responsiveWidth: 900
     });
+
 });
+lightbox.option({
+    'resizeDuration': 100,
+    'wrapAround': true
+})
