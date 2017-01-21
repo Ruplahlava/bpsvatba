d1d0e95029b7836c5b476d042739e6be1bc6c55a
Stepan Postranecky <stepan.postranecky@gmail.com>
2017-01-21 12:21:17 +0100

    Added text content

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         verticalCentered: true,
         menu: '#menu',
-        anchors: ['uvod', 'info', 'doprava', 'pozvanka', 'radost'],
+        anchors: ['uvod', 'info', 'doprava', 'radost', 'pozvanka'],
         navigation: true,
         responsiveWidth: 900
     });
